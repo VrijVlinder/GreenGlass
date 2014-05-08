d6395d8ae6f2a891d2eac805ba23bbd64881a1e1
VrijVlinder <adalib@mac.com>
2014-05-08 15:41:21 -0500

    Updated
    Updated to work with 2.1 version of vanilla

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
 /*
-Copyright 2008, 2009 Vanilla Forums Inc.
+Copyright 2008, 2014 Vanilla Forums Inc.
 This file is part of Garden.
 Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -13,9 +13,9 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
  */
 $ThemeInfo['GreenGlass'] = array(
    'Name' => 'GreenGlass',
-   'Description' => 'A transparent Dark Green theme. This theme is very dynamic and can be placed anywhere. Use it in a Facebook Page Or anywhere you want.',
-   'Version' => '1.3',
-   'Author' => 'VrijVlinder',
+   'Description' => "A transparent Dark Green theme. This theme is very dynamic and can be placed anywhere. Use it in a Facebook Page Or anywhere you want.",
+   'Version' => '1.2',
+   'Author' => "VrijVlinder",
    'AuthorEmail' => 'contact@vrijvlinder.com',
-   'AuthorUrl' => 'vrijvlinder.com'
+   'AuthorUrl' => "http://www.vrijvlinder.com"
 );
--- views/default.master.php
+++ views/default.master.php
@@ -1,18 +1,25 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
-<head> <?php $this->RenderAsset('Head'); ?>
+<head>  
+
+
+
+<?php $this->RenderAsset('Head'); 
+$this->AddJsFile('themes/RedWood/js/jquery.cleditor.min.js');
+?>
+
 <script type="text/javascript">
  var RecaptchaOptions = {
     theme : 'blackglass'
  };
- </script> 
+ </script>
 
+ 
 </head>
 <body id="<?php echo $BodyIdentifier; ?>" class="<?php echo $this->CssClass; ?>">
    <div id="Frame">
-      <div id="Head">
-<h1><span><?php echo Gdn_Theme::Logo(); ?></span></a></h1>
+      <div id="Head"><h1><span><?php echo Gdn_Theme::Logo(); ?></span></a></h1>
          <div class="Menu">
 				
             <?php
@@ -20,20 +27,8 @@
 					if ($this->Menu) {
 						$this->Menu->AddLink('Dashboard', T('Dashboard'), '/dashboard/settings', array('Garden.Settings.Manage'));
 						// $this->Menu->AddLink('Dashboard', T('Users'), '/user/browse', array('Garden.Users.Add', 'Garden.Users.Edit', 'Garden.Users.Delete'));
-						  
-                                                $this->Menu->AddLink('Home', T('Home'), '/');
-                                                 $this->Menu->AddLink('Home', T('New Discussion'), 'post/discussion', array('target'=>'_blank'));
-                                                $this->Menu->AddLink('Home', T('Activity'), '/activity');
-                                                
-                                                
-                                                $this->Menu->AddLink('Home', T('Bonk'), 'plugin/Bonk', array('Garden.Settings.Manage'),array(),array('target'=>'_blank'));
-                                                $this->Menu->AddLink('Home', T('Facebook'), 'http://www.facebook.com/DrugPossessionLaws');
-                                               
-                                                
-                                       
-                                                $this->Menu->AddLink('Home', T('Mobile View'), 'profile/mobile');
-                                                $this->Menu->AddLink('Home', T('Privacy'), 'plugin/PrivacyNotice');
-                                          $Authenticator = Gdn::Authenticator();
+						  $this->Menu->AddLink('Home', T('Home'), '/');
+                                                  $this->Menu->AddLink('Home', T('Activity'), '/activity');
 			         $Authenticator = Gdn::Authenticator();
 						if ($Session->IsValid()) {
 							$Name = $Session->User->Name;
@@ -58,18 +53,8 @@
       </div>
       <div id="Body">
          <div id="Content"><?php $this->RenderAsset('Content'); ?></div>
-         <div id="Panel">
-<!-- Begin TranslateThis Button -->
-
-<div id="translate-this"><a style="width:100%!important;height:25px;display:block;" class="translate-this-button" href="http://www.translatecompany.com/" title="Translate">Translate Company</a></div></br>
-
-<script type="text/javascript" src="http://x.translateth.is/translate-this.js"></script>
-<script type="text/javascript">
-TranslateThis();
-</script>
-
-<!-- End TranslateThis Button -->
-<div class="Search"><p><?php
+         <div id="Panel"><?php $this->AddModule('MeModule');?>
+<div class="Search"><?php
 					$Form = Gdn::Factory('Form');
 					$Form->InputPrefix = '';
 					echo 
@@ -77,82 +62,20 @@ TranslateThis();
 						$Form->TextBox('Search'),
 						$Form->Button('Go', array('Name' => '')),
 						$Form->Close();
-				?></div></p>
-<p><ul id="menu-bar">
- <li><a href="#">I N D E X</a>
-   <ul>
- 
-   <li><a href="http://vanillaforums.org/" target="_blank">Vanilla</a></li>
-   <li><a href="http://www.drugpossessionlaws.com/" target="_blank">Main Site</a></li>
-   <li><a href="http://www.facebook.com/DrugPossessionLaws/" target="_blank">Facebook</a></li>
-   <li><a href="https://www.facebook.com/DrugPossessionLaws/app_190322544333196/" target="_blank">Facebook Forum</a></li>
-  </ul>
- </li>
- </ul>
-</p>
+				?></div>
+
+
 <?php $this->RenderAsset('Panel'); ?>
-<center><p><img src="http://www.drugpossessionlaws.com/forum/themes/GreenGlass/design/images/logo1.png" width="250" height="auto"/></p><p>
-<iframe id="onlineRadioFrame" frameborder="0" width="250" height="292" scrolling="no" src="http://radiotuna.com/OnlineRadioPlayer/Player?showPopupControl=true&amp;playerParams={'styleSelection0':29,'styleSelection1':2,'styleSelection2':7,'textColor':12517120,'backgroundColor':2314512,'buttonColor':10419978,'glowColor':10419978,'playerSize':250,'playerType':'style'}&amp;linkText=internet%20radio&amp;linkDest=http://radiotuna.com/" allowtransparency="true" style="border:none;border-radius:10px;"></iframe></p>
- </center>     </div>
-      <div id="Foot">
-<div id="FootMenu">
-				
-            <?php
-			      $Session = Gdn::Session();
-					if ($this->Menu) {
-						$this->Menu->AddLink('Categories', T('Categories'), 'categories/all');
-                                                $this->Menu->AddLink('Discussions', T('Discussions'), '/discussions');
-                                                $this->Menu->AddLink('Home', T('Home'), '/');
-                                                $this->Menu->AddLink('New Discussion',T('New Discussion'),'/post/discussion', array('Garden.SignIn.Allow'), array(), array('target' => '_blank')); 
-                                                  
-                                                $this->Menu->AddLink('Mobile View', T('Mobile View'), 'profile/mobile');
-                                                
-                                                $this->Menu->AddLink('Facebook', T('Facebook'), 'http://www.facebook.com/DrugPossessionLaws');
-                                                
-                                                
-			         }echo $this->Menu->ToString();?>
-				
-           
-         </div></br>
 
-<p id="back-top" style="float: right;top:0px; display: inline-block; position:relative;margin-right:0px;"><a href="#top" title="Back to Top"><img src="http://vrijvlinder.com/forum/themes/VrijVlinder/design/images/back-up1.png"></a></p>
-   <?php $this->RenderAsset('Foot'); 
-echo Wrap(Anchor(T('Vanilla Theme by VrijVlinder'), C('Garden.VanillaUrl'),array('target' => '_blank')), 'div');	
-?></div>
+</div>
+      </div>
+      
+</div>
+
+   </div>
 <script type="text/javascript"> $(document).ready(function() {
-    $(".Attachment a").attr("target", '_blank');
-   $("#Foot #Menu a").attr("target", '_blank');
-   $(".AptAdImg a").attr("target", '_blank');
-  $('#Menu ul li:nth-child(9) > a').popup(); 
-
- $("#Foot #Menu li:last > a").popup();
-
-});
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-
-	$("#back-top").hide();
-	
-	// fade in #back-top
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
-
-		// scroll body to 0px on click
-		$('#back-top a').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	});
-
+   $(".Attachment a").attr("target", '_blank');
+   
 });
 </script>
 <script type="text/javascript">
@@ -173,8 +96,9 @@ jQuery(document).ready(function()
 });
 
 document.onclick = Menu_close;</script>
-
+<div id="Foot">
+<div id="FootMenu"></div>
+<?php $this->RenderAsset('Foot'); echo Wrap(Anchor(T('Vanilla Theme by VrijVlinder'), C('Garden.VanillaUrl'),array('target' => '_blank')), 'div');?> </div>
 	<?php $this->FireEvent('AfterBody'); ?>
 </body>
-
 </html>
